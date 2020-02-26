c87f644148ac867e4981bf83e8a6c1277daeb6c6
Isaac Prior <isaac@stackhpc.com>
2020-02-26 15:56:58 +0000

    Pin to jdk11

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 cd $(dirname $0)
 build_date=$(date +%Y%m%d-%H%M%S)
-docker build -t localhost:5000/jenkins-docker --build-arg "BUILD_DATE=$build_date" .
+jenkins_ver=jdk11
+docker build -t localhost:5000/jenkins-docker --build-arg "BUILD_DATE=$build_date" \
+       --build-arg "JENKINS_VER=$jenkins_ver" .
 
