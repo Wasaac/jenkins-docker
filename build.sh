92c2ca5dfbe285eb66c3cb6a14cba29cb771ef51
Isaac Prior <isaac@stackhpc.com>
2020-03-27 13:08:59 +0000

    Removed registry location assumption from build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 cd $(dirname $0)
 build_date=$(date +%Y%m%d-%H%M%S)
 jenkins_ver=jdk11
-docker build -t localhost:5000/jenkins-docker --build-arg "BUILD_DATE=$build_date" \
+docker build -t jenkins-docker --build-arg "BUILD_DATE=$build_date" \
        --build-arg "JENKINS_VER=$jenkins_ver" .
 
